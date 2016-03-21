3323585c7c8e450be059d2544896288b8767b977
Chuck Reynolds <chuck@vuurr.com>
2016-03-20 18:26:47 -0700

    quick clean main pagebuilder the_content out

--- tpl-pagebuilder.php
+++ tpl-pagebuilder.php
@@ -12,25 +12,11 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-<?php
-/**
- * run the content types hyperloop
- */
-do_action( 'mindup_hyperloop' ); ?>
-
-
-<?php
-// while ( have_posts() ) : the_post();
-
-// 	get_template_part( 'template-parts/content', 'page' );
-
-// 	// If comments are open or we have at least one comment, load up the comment template.
-// 	if ( comments_open() || get_comments_number() ) :
-// 		comments_template();
-// 	endif;
-
-// endwhile; // End of the loop.
-?>
+		<?php
+		/**
+		 * run the content types hyperloop
+		 */
+		do_action( 'mindup_hyperloop' ); ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
