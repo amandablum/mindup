44c4c3142670586d3881d88b6d7ee5dc9c2fb8d1
Chuck Reynolds <chuck@vuurr.com>
2017-03-29 13:36:27 -0700

    version bump for new deploy, touch #138 #139
    v1.1.0

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  * Set theme version for caching.
  * If you remove this the world will implode. dont.
  */
-define( 'MINDUP_VERSION', '1.0.0' );
+define( 'MINDUP_VERSION', '1.1.0' );
 
 
 if ( ! function_exists( 'mindup_setup' ) ) :
